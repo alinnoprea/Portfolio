4764fefe7af072599c9275d51eac04620e038418
Alin Oprea <opreaalin2001@gmail.com>
2023-11-12 18:30:50 +0200

    Finished mostly

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -4,7 +4,7 @@ const About = forwardRef((props, ref) => {
   return (
     <div ref={ref}>
       <div className=" bg-primary h-screen w-screen flex items-center justify-center snap-center">
-        <h1 className="text-secondary border-black w-5/6 text-4xl flex flex-wrap text-center animate-fade-right animate-once animate-ease-in-out">
+        <h1 className="text-secondary border-black w-5/6 text-4xl flex flex-wrap text-center animate-fade-right animate-once animate-ease-in-out ">
           My journey into web development began in 2021, where I took my first
           steps with Pluralsight and FreeCodeCamp. I&apos;m a enthusiast
           who&apos;s passionate about coding and eager to share my evolving
--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -6,7 +6,10 @@ const Home = forwardRef((props, ref) => {
     <div ref={ref}>
       <div className=" bg-primary h-screen w-screen flex items-center justify-center snap-center">
         <div className="animate-fade-right animate-once animate-ease-in-out text-secondary text-9xl text-center  border-black w-5/6 ">
-          <h1>Hi, I&apos;m Alin! I&apos;m a Web Developer.</h1>
+          <h1>
+            Hi, I&apos;m Alin!
+            <br />A Web Developer.
+          </h1>
         </div>
       </div>
     </div>
